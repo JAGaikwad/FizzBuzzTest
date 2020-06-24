b35901184f0c70b9c1fd02137e0ae64dfa6f9f4d
Jyoti <jgaikwad31@gmail.com>
2020-06-24 19:51:23 +0530

    FizzBuzzTest Final Version, Tested OK

--- FizzBuzzTest.ino
+++ FizzBuzzTest.ino
@@ -1,49 +1,26 @@
-#define MODE_FIZZBUZZ    (1)
-#define MODE_FIZZ        (2)
-#define MODE_BUZZ        (3)
-#define MODE_OTHER       (4)
-
 #define WAIT_TIME        (10) // [ms]
-int g_count = 0;
-int g_display_mode;
+int g_count;
 
 void setup() {
   Serial.begin(115200);
+  g_count=1;
 }
 
 void loop() {
-  if(Serial.available() > 0) {
-    for(g_count=1;g_count>101;g_count++) {
-      if(g_count%15 == 0) {
-            g_display_mode = MODE_FIZZBUZZ;
-        }
-      else if(g_count%5 == 0) {
-            g_display_mode = MODE_FIZZ;
-        }
-      else if(g_count%3 == 0){
-            g_display_mode = MODE_BUZZ;
-        }
-      else {
-            g_display_mode = MODE_OTHER;
-        }
-      }  
-    }
-
-      if(g_display_mode == MODE_FIZZBUZZ) {
-          Serial.println(g_count); // output number         
-          Serial.print(" : FizzBuzz"); // output :FizzBuzz
-        }
-        else if(g_display_mode == MODE_FIZZ) {
-          Serial.println(g_count); // output number  
-          Serial.print(" : Fizz"); // output :Fizz
-        }
-        else if(g_display_mode == MODE_BUZZ){
-          Serial.println(g_count); // output number  
-          Serial.print(" : Buzz"); // output :Buzz
-        }
-        else if(g_display_mode == MODE_OTHER){
-          Serial.println(g_count); // output number
-        }
-
-    delay(WAIT_TIME);
+      while(g_count<101)
+      {
+        Serial.print(g_count);
+        if(g_count%15 == 0) {
+             Serial.print(" : FizzBuzz"); // output :FizzBuzz
+          }
+        else if(g_count%5 == 0) {
+             Serial.print(" : Fizz"); // output :Fizz
+          }
+        else if(g_count%3 == 0){
+            Serial.print(" : Buzz"); // output :Buzz
+          }
+        Serial.print("\n");
+          g_count++;
+        delay(WAIT_TIME);
+   }
 }
